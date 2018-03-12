a35cae860f9064b60b5c8f280bba52fc7457f3c2
Shih, yumei | Yumi | TWR <yumei.shih@rakuten.com>
2018-03-12 17:50:57 +0800

    Modify model useing map and ternary operator

--- app/models/item_list.rb
+++ app/models/item_list.rb
@@ -1,12 +1,6 @@
 class ItemList < ApplicationRecord
   attribute :isNew
   def self.add_new_label(itemList)
-    itemList.each do |item|
-    if item.created_at > (Time.now - 12.hours)
-      item.isNew = true
-    else
-      item.isNew = false
-    end
-    end
+    itemList.map do |item| item.isNew = item.created_at > (Time.now - 12.hours)? true : false end
   end
 end
